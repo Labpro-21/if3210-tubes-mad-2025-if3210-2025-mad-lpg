a132d53919f39046917af9fd46995153cab70a80
BryanLauw <13522033@std.stei.itb.ac.id>
2025-04-12 16:17:45 +0700

    fix: fix refresh token use case error

--- app/src/main/java/com/tubes1/purritify/features/auth/domain/usecase/auth/ReadTokenUseCase.kt
+++ app/src/main/java/com/tubes1/purritify/features/auth/domain/usecase/auth/ReadTokenUseCase.kt
@@ -1,6 +1,7 @@
 package com.tubes1.purritify.features.auth.domain.usecase.auth
 
 import android.util.Log
+import com.tubes1.purritify.core.common.utils.Resource
 
 class ReadTokenUseCase(
     private val getAccessTokenUseCase: GetAccessTokenUseCase,
@@ -8,8 +9,8 @@ class ReadTokenUseCase(
     private val verifyTokenUseCase: VerifyTokenUseCase,
     private val refreshTokenUseCase: RefreshTokenUseCase,
 ) {
-    suspend fun invoke(): String {
-        val currentAccessToken = getAccessTokenUseCase()
+    suspend operator fun invoke(): String {
+        var currentAccessToken = getAccessTokenUseCase()
         if (verifyTokenUseCase(currentAccessToken)) {
             Log.d("TOKEN_STATUS", "Case 1: Akses token belum expired")
             return currentAccessToken
@@ -18,8 +19,18 @@ class ReadTokenUseCase(
         val currentRefreshToken = getRefreshTokenUseCase()
         if (verifyTokenUseCase(currentRefreshToken)) {
             Log.d("TOKEN_STATUS", "Case 2: Akses token expired, tapi Refresh token belum")
-            refreshTokenUseCase(currentRefreshToken)
-            return getAccessTokenUseCase()
+            refreshTokenUseCase(currentRefreshToken).collect { resource ->
+                when(resource) {
+                    is Resource.Success -> {
+                        currentAccessToken = resource.data?.accessToken ?: ""
+                    } is Resource.Error -> {
+                        Log.e("TOKEN_REFRESH_ERROR", resource.message ?: "Unknown error")
+                    } else -> {
+
+                    }
+                }
+            }
+            return currentAccessToken
         }
 
         Log.d("TOKEN_STATUS", "Case 3: Dua-duanya expired")
